3e16afc082023717b34c9f9bf8923c0e42520e59
Anastasios Bourazanis <a.bourazanis@gmail.com>
2011-11-15 11:25:12 +0200

    sdcard mount fix and init.rc clean up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,9 +60,8 @@ WITH_JIT := true
 ENABLE_JSC_JIT := true
 
 # USB mass storage
-BOARD_CUSTOM_USB_CONTROLLER := ../../device/huawei/u8120/UsbController.cpp
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun"
+BOARD_CUSTOM_USB_CONTROLLER := ../../device/huawei/u8120/UsbController.cpp
 BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
 
 # Sensors
